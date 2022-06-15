390c82c2abe53785fafd382c41de197ce89c54fd
Yejin Song <yejsong@c5r2s2.42seoul.kr>
2022-06-15 13:49:51 +0900

    add nickname condition check

--- backend/src/nickname/nickname.controller.ts
+++ backend/src/nickname/nickname.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get, Param } from "@nestjs/common";
+import { Controller, Get, Param, ValidationPipe } from "@nestjs/common";
 import { NicknameService } from "./nickname.service";
+import { NicknameCredentialsDto } from "./dto/nickname-credential.dto";
 
 @Controller("nickname")
 export class NicknameController {
@@ -7,8 +8,8 @@ export class NicknameController {
 
   @Get("/:nickname")
   isExistNickname(
-    @Param("nickname") nickname: string,
+    @Param(ValidationPipe) nicknameCredentialsDto: NicknameCredentialsDto,
   ): Promise<{ isExistNickname: boolean }> {
-    return this.nicknameService.isExistNickname(nickname);
+    return this.nicknameService.isExistNickname(nicknameCredentialsDto);
   }
 }
--- backend/src/nickname/nickname.service.ts
+++ backend/src/nickname/nickname.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { UserRepository } from "src/user/user.repository";
 import { User } from "src/user/user.entity";
+import { NicknameCredentialsDto } from "./dto/nickname-credential.dto";
 
 @Injectable()
 export class NicknameService {
@@ -10,8 +11,9 @@ export class NicknameService {
     private userRepository: UserRepository,
   ) {}
   async isExistNickname(
-    nickname: string,
+    nicknameCredentialsDto: NicknameCredentialsDto,
   ): Promise<{ isExistNickname: boolean }> {
+    const { nickname } = nicknameCredentialsDto;
     const found: User = await this.userRepository.findOne({ nickname });
     if (!found) return { isExistNickname: true };
     return { isExistNickname: false };
