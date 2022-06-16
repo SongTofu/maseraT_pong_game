9e9bd11c4ed582ac210de45f4c1c6de6c1fb9176
Yejin Song <yejsong@c5r2s2.42seoul.kr>
2022-06-16 14:20:48 +0900

    Grouping getMyInfo function return value

--- backend/src/user-info/user-info.controller.ts
+++ backend/src/user-info/user-info.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from "@nestjs/common";
+import { UserInfoService } from "./user-info.service";
+import { MyUserInfoDto } from "./dto/my-user-info.dto";
 
-@Controller('user-info')
-export class UserInfoController {}
+@Controller("user-info")
+export class UserInfoController {
+  constructor(private userInfoService: UserInfoService) {}
+
+  @Get()
+  getMyInfo(): Promise<MyUserInfoDto> {
+    return this.userInfoService.getMyInfo(1);
+  }
+}
--- backend/src/user-info/user-info.service.ts
+++ backend/src/user-info/user-info.service.ts
@@ -1,4 +1,24 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from "@nestjs/common";
+import { UserRepository } from "./user.repository";
+import { MyUserInfoDto } from "./dto/my-user-info.dto";
 
 @Injectable()
-export class UserInfoService {}
+export class UserInfoService {
+  constructor(private userRepository: UserRepository) {}
+  async getMyInfo(id: number): Promise<MyUserInfoDto> {
+    const user = await this.userRepository.findOne(id);
+
+    const myUserInfoDto: MyUserInfoDto = {
+      nickname: user.nickname,
+      secondAuth: user.secondAuth,
+      pWin: user.pWin,
+      pLose: user.pLose,
+      profileImg: user.profileImg,
+      rWin: user.rWin,
+      rLose: user.rLose,
+      level: user.level,
+    };
+    console.log("info", myUserInfoDto);
+    return myUserInfoDto;
+  }
+}
