087f4564593536afb2a1096a128b509b7924a866
Ji Hae Kim (ji-kim) <53713342+deftones88@users.noreply.github.com>
2022-06-19 16:51:24 +0900

    Feat : frontend react settings
    - eslint
    - prettier
    - tailwindcss

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-6xl text-center">TEST</h1>
+    </div>
+  );
+}
+
+export default App;
