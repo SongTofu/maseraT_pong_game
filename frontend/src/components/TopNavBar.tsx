0ff3fd706d9a1d800cf033c406ab267d58e1929c
Ji Hae Kim (ji-kim) <53713342+deftones88@users.noreply.github.com>
2022-06-27 22:00:12 +0900

    [Feat] ui set up & [Fix] topNavBar
    - chatroom ui set up
    - fix topNavBar max width

--- frontend/src/components/TopNavBar.tsx
+++ frontend/src/components/TopNavBar.tsx
@@ -31,131 +31,138 @@ function TopBar({ children }: Props) {
 
   return (
     <div>
-      <ul className="h-15 w-full bg-main flex flex-row items-end">
-        <li
-          className={`btn-nav border-x-2 ${
-            window.location.pathname === "/game" ||
-            window.location.pathname === "/game-room"
-              ? ""
-              : "border-b-2"
-          }
+      <div className="flex justify-center bg-main relative">
+        <div className="max-w-5xl bg-main z-40">
+          <ul className="h-15 flex items-end ">
+            <li
+              className={`btn-nav border-x-2 ${
+                window.location.pathname === "/game" ||
+                window.location.pathname === "/game-room"
+                  ? ""
+                  : "border-b-2"
+              }
         `}
-        >
-          <h1
-            className={`font-nav ${
-              window.location.pathname === "/game" ||
-              window.location.pathname === "/game-room"
-                ? "text-main"
-                : "text-gray-300"
-            }`}
-          >
-            <NavLink to="/game">게임</NavLink>
-          </h1>
-        </li>
-        <li
-          className={`btn-nav border-r-2 ${
-            window.location.pathname === "/chat" ||
-            window.location.pathname === "/chat-room"
-              ? ""
-              : "border-b-2"
-          }
-        `}
-        >
-          <h1
-            className={`font-nav pl-11 ${
-              window.location.pathname === "/chat" ||
-              window.location.pathname === "/chat-room"
-                ? "text-main"
-                : "text-gray-300"
-            }`}
-          >
-            <NavLink to="/chat">채팅</NavLink>
-          </h1>
-        </li>
-        <li>
-          <div className="h-12 w-[calc(100vw_-_300px)] bg-main-light flex justify-between px-3 items-center border-b-2 border-main">
-            <div className="text-main-text flex flex-row">
-              <p className="pr-2">player name</p>
-              <div
-                onMouseEnter={handleMouseEnter}
-                onMouseLeave={handleMouseLeave}
-                className="relative"
+            >
+              <h1
+                className={`font-nav ${
+                  window.location.pathname === "/game" ||
+                  window.location.pathname === "/game-room"
+                    ? "text-main"
+                    : "text-gray-300"
+                }`}
               >
-                <img
-                  alt="achievement icon"
-                  src={AchievementIcon}
-                  className="w-5"
-                />
-                {showAchievement && (
+                <NavLink to="/game">게임</NavLink>
+              </h1>
+            </li>
+            <li
+              className={`btn-nav border-r-2 ${
+                window.location.pathname === "/chat" ||
+                window.location.pathname === "/chat-room"
+                  ? ""
+                  : "border-b-2"
+              }
+        `}
+            >
+              <h1
+                className={`font-nav  ${
+                  window.location.pathname === "/chat" ||
+                  window.location.pathname === "/chat-room"
+                    ? "text-main"
+                    : "text-gray-300"
+                }`}
+              >
+                <NavLink to="/chat">채팅</NavLink>
+              </h1>
+            </li>
+            <li>
+              <div className="h-12 w-[800px] bg-main-light flex justify-between px-3 items-center border-b-2 border-main">
+                <div className="text-main-text flex flex-row">
+                  <p className="pr-2">player name</p>
                   <div
-                    className="w-[150px] h-[30px] bg-white border-black rounded-md border-[1px] absolute top-[-20px] left-[10px] flex justify-evenly items-center"
                     onMouseEnter={handleMouseEnter}
+                    onMouseLeave={handleMouseLeave}
+                    className="relative"
                   >
                     <img
-                      alt="첫 로그인"
-                      title="첫 로그인"
-                      src={FirstLogin}
-                      className="w-[20px] h-[20px]"
+                      alt="achievement icon"
+                      src={AchievementIcon}
+                      className="w-5"
                     />
-                    <img
-                      alt="첫승"
-                      title="첫승"
-                      src={FirstWin}
-                      className="w-[20px] h-[20px]"
+                    {showAchievement && (
+                      <div
+                        className="w-[150px] h-[30px] bg-white border-black rounded-md border-[1px] absolute top-[-20px] left-[10px] flex justify-evenly items-center"
+                        onMouseEnter={handleMouseEnter}
+                      >
+                        <img
+                          alt="첫 로그인"
+                          title="첫 로그인"
+                          src={FirstLogin}
+                          className="w-[20px] h-[20px]"
+                        />
+                        <img
+                          alt="첫승"
+                          title="첫승"
+                          src={FirstWin}
+                          className="w-[20px] h-[20px]"
+                        />
+                        <img
+                          alt="3승"
+                          title="3승"
+                          src={ThirdWin}
+                          className="w-[20px] h-[20px]"
+                        />
+                        <img
+                          alt="첫패"
+                          title="첫패"
+                          src={FirstLose}
+                          className="w-[20px] h-[20px]"
+                        />
+                        <img
+                          alt="3연승"
+                          title="3연승"
+                          src={ConsecThree}
+                          className="w-[20px] h-[20px]"
+                        />
+                      </div>
+                    )}
+                  </div>
+                </div>
+                <div className="w-[500px] text-main-text flex justify-between items-center">
+                  <p className="inline">lv. {0}</p>
+                  <div className="inline">
+                    <span className="block text-xs">일반/레더전적</span>
+                    <span className="block text-sm ">
+                      {0}승 {0}패/{0}승 {0}패
+                    </span>
+                  </div>
+                  <div className="flex">
+                    <ButtonTwo
+                      tag="프로필 보기"
+                      className="inline text-sm mr-2"
+                      onClick={() => handleOptionChange(openModal)}
                     />
-                    <img
-                      alt="3승"
-                      title="3승"
-                      src={ThirdWin}
-                      className="w-[20px] h-[20px]"
+                    <ButtonTwo
+                      tag="닉네임 변경"
+                      className="inline text-sm mr-2"
+                      onClick={() => {
+                        handleOptionChange(openModal);
+                      }}
                     />
-                    <img
-                      alt="첫패"
-                      title="첫패"
-                      src={FirstLose}
-                      className="w-[20px] h-[20px]"
-                    />
-                    <img
-                      alt="3연승"
-                      title="3연승"
-                      src={ConsecThree}
-                      className="w-[20px] h-[20px]"
+                    <ButtonTwo
+                      tag="2차 인증 활성화"
+                      className="inline text-sm "
+                      onClick={() => {
+                        handleOptionChange(openModal);
+                      }}
                     />
                   </div>
-                )}
+                </div>
               </div>
-            </div>
-            <div className="w-[500px] text-main-text flex justify-between items-center">
-              <p className="inline">lv. 0</p>
-              <div className="inline">
-                <span className="block text-xs">일반/레더전적</span>
-                <span className="block text-sm ">0승 0패/0승 0패</span>
-              </div>
-              <div className="flex">
-                <ButtonTwo
-                  tag="프로필 보기"
-                  className="inline text-sm mr-2"
-                  onClick={() => handleOptionChange(openModal)}
-                />
-                <ButtonTwo
-                  tag="닉네임 변경"
-                  className="inline text-sm mr-2"
-                  onClick={() => {
-                    handleOptionChange(openModal);
-                  }}
-                />
-                <ButtonTwo
-                  tag="2차 인증 활성화"
-                  className="inline text-sm "
-                  onClick={() => {
-                    handleOptionChange(openModal);
-                  }}
-                />
-              </div>
-            </div>
-          </div>
-        </li>
-      </ul>
+            </li>
+          </ul>
+        </div>
+        <div className="bg-main-light w-[50%] h-12 absolute top-2 right-0 border-b-2 border-main z-10"></div>
+      </div>
       {openModal && <TestModal />}
 
       {children}
