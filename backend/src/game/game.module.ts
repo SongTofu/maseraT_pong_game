ab597ae32d37c5d9c34a47c88ffbe394a8d070af
Yeshin Lee <yeslee5004@gmail.com>
2022-07-05 23:11:54 +0900

    [Feat] craete initial socket: join game room
    - create dto, controller, service
    - create method(createRoom) in repository

--- backend/src/game/game.module.ts
+++ backend/src/game/game.module.ts
@@ -3,11 +3,14 @@ import { GameGateway } from "./game.gateway";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { GameRoomRepository } from "./repository/game-room.repository";
 import { GameParticipantRepository } from "./repository/game-participant.repository";
+import { GameController } from './game.controller';
+import { GameService } from './game.service';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([GameRoomRepository, GameParticipantRepository]),
   ],
-  providers: [GameGateway],
+  providers: [GameGateway, GameService],
+  controllers: [GameController],
 })
 export class GameModule {}
--- backend/src/game/repository/game-room.repository.ts
+++ backend/src/game/repository/game-room.repository.ts
@@ -1,5 +1,18 @@
 import { Repository, EntityRepository } from "typeorm";
+import { GameJoinDto } from "../dto/game-room.dto";
 import { GameRoom } from "../entity/game-room.entity";
 
 @EntityRepository(GameRoom)
-export class GameRoomRepository extends Repository<GameRoom> {}
+export class GameRoomRepository extends Repository<GameRoom> {
+  async createRoom(gameJoinDto: GameJoinDto): Promise<number> {
+    const { title } = gameJoinDto;
+
+    const gameRoom: GameRoom = this.create({
+      title,
+    });
+
+    const saveRoom = await gameRoom.save();
+
+    return saveRoom.id;
+  }
+}
