614b0b80533840927c80a6eb8191b298eb6268c1
Jeongwook Lee <jeongwle@c2r9s8.42seoul.kr>
2022-07-08 18:20:55 +0900

    [Modified] : hide Token - for security, add token to .env

--- frontend/src/api/getApi.ts
+++ frontend/src/api/getApi.ts
@@ -3,8 +3,8 @@ import axios from "axios";
 export async function getApi(api: string) {
   const config = {
     headers: {
-      Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NywiaWF0IjoxNjU3MDg3OTMwLCJleHAiOjE2ODg2MjM5MzB9.vedsKRI96GBQFNsNW05lalMGB4Va9whCmzxbnd5xzP4`,
-      // Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NiwiaWF0IjoxNjU3MjU2MTAzLCJleHAiOjE2ODg3OTIxMDN9.h0Ao071lHRnrLTEbdDMlLQI0XZ3etODmwS-tVXnBWjw`,
+      Authorization: `Bearer ${process.env.REACT_APP_MY_TOKEN}`,
+      // Authorization: `Bearer ${process.env.REACT_APP_SEHAN_TOKEN}`,
     },
   };
 
