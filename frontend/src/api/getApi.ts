d93cf9232fb4d561f89a895d3905aaad44749c38
Jeongwook Lee <jeongwle@c2r9s8.42seoul.kr>
2022-07-08 17:09:15 +0900

    [Modified] : just add sehan's token - for check by login another id

--- frontend/src/api/getApi.ts
+++ frontend/src/api/getApi.ts
@@ -4,6 +4,7 @@ export async function getApi(api: string) {
   const config = {
     headers: {
       Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NywiaWF0IjoxNjU3MDg3OTMwLCJleHAiOjE2ODg2MjM5MzB9.vedsKRI96GBQFNsNW05lalMGB4Va9whCmzxbnd5xzP4`,
+      // Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NiwiaWF0IjoxNjU3MjU2MTAzLCJleHAiOjE2ODg3OTIxMDN9.h0Ao071lHRnrLTEbdDMlLQI0XZ3etODmwS-tVXnBWjw`,
     },
   };
 
