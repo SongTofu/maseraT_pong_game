d25413303025bf40bfdb56635e70204df048483e
Jeongwook Lee <jeongwle@c2r8s8.42seoul.kr>
2022-07-10 19:32:54 +0900

    [Modify] : apply PopUpRoomSet - MainBox : later, we need to make something to distinguish make room and set room - UserListBox : if owner, can modify room setting.

--- frontend/src/components/Content/MainBox.tsx
+++ frontend/src/components/Content/MainBox.tsx
@@ -1,10 +1,13 @@
-import React from "react";
+import ClickAwayListener from "@mui/material/ClickAwayListener";
+import React, { useState } from "react";
 import { useRecoilValue } from "recoil";
 import shortid from "shortid";
 import { getChatRoom, IChatRoom } from "../../state/getChRoom";
 import ButtonTwo from "../Button/ButtonTwo";
 import ChatRoomList from "../List/ChatRoomList";
 import GameRoomList from "../List/GameRoomList";
+import PopUpParent from "../PopUp/PopUpParent";
+import PopUpRoomSet from "../PopUp/PopUpRoomSet";
 
 interface IProps {
   buttonTag: string;
@@ -13,14 +16,49 @@ interface IProps {
 
 function MainBox({ buttonTag, isGame }: IProps) {
   const chatRooms = useRecoilValue<IChatRoom[]>(getChatRoom);
+  const [openModal, setOpenModal] = useState(false);
+
+  const handleOptionChange = () => setOpenModal((prev) => !prev);
   return (
     <div className="content-box w-[550px] mr-3 my-5">
       <div className="w-[90%] flex justify-end pt-4 pb-4">
         <ButtonTwo
           tag={buttonTag}
           className="px-4 tracking-widest text-sm font-main"
+          onClick={handleOptionChange}
         />
       </div>
+      {openModal && buttonTag === "채팅방 만들기" && (
+        <ClickAwayListener onClickAway={() => setOpenModal(false)}>
+          {/* onclick으로 전달해주어서 방설정과 방만들기 구분하기??? 일단 드는생각적어봄 */}
+          <div>
+            <PopUpParent
+              width={"w-[400px]"}
+              height={"h-[500px]"}
+              mainText={"채팅방 만들기"}
+              onClick={handleOptionChange}
+            >
+              <PopUpRoomSet />
+            </PopUpParent>
+          </div>
+        </ClickAwayListener>
+      )}
+      {openModal && buttonTag === "방 만들기" && (
+        <ClickAwayListener onClickAway={() => setOpenModal(false)}>
+          {/* onclick으로 전달해주어서 방설정과 방만들기 구분하기??? 일단 드는생각적어봄 */}
+          <div>
+            <PopUpParent
+              width={"w-[400px]"}
+              height={"h-[500px]"}
+              mainText={"게임방 만들기"}
+              onClick={handleOptionChange}
+            >
+              {/* 얘는 임시로 넣어논 것 게임방만들기 컴포넌트 새로 만들어야할지도? */}
+              <PopUpRoomSet />
+            </PopUpParent>
+          </div>
+        </ClickAwayListener>
+      )}
       <div className="h-full w-full flex flex-col items-center mb-3 overflow-y-scroll">
         {isGame ? (
           <>
