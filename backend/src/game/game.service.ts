31c42a9be90d500ee0469e226abbfb1c478aec6f
Yejin Song <yejsong@c2r10s4.42seoul.kr>
2022-07-28 15:05:23 +0900

    [Feat] gameroom에서 보여줄 사용자 프로필 보내기

--- backend/src/game/game.service.ts
+++ backend/src/game/game.service.ts
@@ -5,6 +5,7 @@ import { GameParticipant } from "./entity/game-participant.entity";
 import { GameParticipantDto } from "./dto/game-participant.dto";
 import { GameRoomDetailDto } from "./dto/game-room-detail.dto";
 import { GameRoom } from "./entity/game-room.entity";
+import { GameParticipantProfile } from "./dto/game-participant-profile.dto";
 
 @Injectable()
 export class GameService {
@@ -18,26 +19,54 @@ export class GameService {
   }
 
   async gameRoomDetail(gameRoomId: number): Promise<GameRoomDetailDto> {
-    const gameParticipants: GameParticipant[] =
-      await this.gameParticipantRepository.find({
-        where: { gameRoom: gameRoomId },
+    // const gameParticipants: GameParticipant[] =
+    //   await this.gameParticipantRepository.find({
+    //     where: { gameRoom: gameRoomId },
+    //     relations: ["user"],
+    //   });
+
+    const leftgameParticipant: GameParticipant =
+      await this.gameParticipantRepository.findOne({
+        where: { gameRoom: gameRoomId, position: 0 },
+        relations: ["user"],
+      });
+
+    const rightgameParticipant: GameParticipant =
+      await this.gameParticipantRepository.findOne({
+        where: { gameRoom: gameRoomId, position: 1 },
         relations: ["user"],
       });
 
     const gameRoom: GameRoom = await this.gameRoomRepository.findOne(
       gameRoomId,
     );
-    const gameParticipantDto: GameParticipantDto[] = [];
+    // const gameParticipantDto: GameParticipantDto[] = [];
 
-    gameParticipants.forEach((gameParticipant) => {
-      gameParticipantDto.push(new GameParticipantDto(gameParticipant));
-    });
-
+    // gameParticipants.forEach((gameParticipant) => {
+    //   gameParticipantDto.push(new GameParticipantDto(gameParticipant));
+    // });
+    // const gameParticipantProfile :
     const gameRoomDetailDto: GameRoomDetailDto = {
       gameRoomId: gameRoomId,
       title: gameRoom.title,
-      gameParticipant: gameParticipantDto,
+      gameUser: [],
     };
+
+    if (!leftgameParticipant)
+      gameRoomDetailDto.gameUser.push(new GameParticipantProfile(null, 0));
+    else {
+      gameRoomDetailDto.gameUser.push(
+        new GameParticipantProfile(leftgameParticipant.user, 0),
+      );
+    }
+    if (!rightgameParticipant)
+      gameRoomDetailDto.gameUser.push(new GameParticipantProfile(null, 0));
+    else {
+      gameRoomDetailDto.gameUser.push(
+        new GameParticipantProfile(rightgameParticipant.user, 1),
+      );
+    }
+
     return gameRoomDetailDto;
   }
 }
